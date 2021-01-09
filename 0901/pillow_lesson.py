ca536c7cf05698cc4fd8d7f67489417e2e3e96e1
demidraksin <greatraksin@icloud.com>
2021-01-09 16:57:25 +0300

    Adding pillow theory

--- 0901/pillow_lesson.py
+++ 0901/pillow_lesson.py
@@ -1,4 +1,17 @@
-from PIL import Image
+from PIL import Image, ImageFilter
+
 
 sample = Image.open('img.jpg')
-sample.show()
+# sample.show() - показать изображение
+resized_image = sample.resize((1280, 720))  # изменить размер изображения (ширина, высота)
+resized_image.save('resized.jpg')  # сохранить изображение ('имя.расширение')
+
+''' Создать 360 копий изображения, повернуть их на один градус (каждую)
+for angle in range(1, 361):
+    image_name = 'rotated_' + str(angle) + '.jpg'
+    new_image = resized_image.rotate(angle)
+    new_image.save('./rotation/' + image_name)
+'''
+
+edges = resized_image.filter(ImageFilter.FIND_EDGES)
+edges.show()
