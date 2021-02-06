d6c3842fafb08cc61d4582da1efc13714924c7ed
demidraksin <greatraksin@icloud.com>
2021-02-06 16:42:24 +0300

    Reformatting database connection function, adding create table function🦠

--- 0602/t_3_databases.py
+++ 0602/t_3_databases.py
@@ -3,20 +3,31 @@ from sqlite3 import Error
 
 
 def create_connection(db_file):
+    """ создает подключение к БД, БД передается параметром
+    :param db_file - файл БД, к которой нужно подключаться
+    :return Объект подключения или None
+    """
     connection = None
     try:
         connection = sqlite3.connect(db_file)
         '''Пробую подключиться к базе, название
         которой мне передает пользователь. В случае
         успеха сообщу об этом'''
-        print('Подключено!')
+        return connection
     except Error as e:
         '''Если возникает ошибка, я хочу ее распечатать'''
         print(e)
-    finally:
-        if connection:
-            '''Если у меня все получилось, закрываю подключение'''
-            connection.close()
+
+    return connection
 
 
-create_connection('test.db')
+def create_table(conn, create_table_SQL):
+    """ создает таблицу
+    :param conn - к чему подключаемся
+    :param create_table_SQL - запрос на создание таблицы
+    """
+    try:
+        c = conn.cursor() # пытаюсь создать манипулятор для БД
+        c.execute(create_table_SQL)  # если получается, направляю запрос через .execute()
+    except Error as e:
+        print(e)
