08087740c9334669b6e33e1f56f170be57372d42
demidraksin <greatraksin@icloud.com>
2021-05-22 16:52:59 +0300

    Adding new function for alarm👀

--- 00_projects/2205_digital_clock/clock.py
+++ 00_projects/2205_digital_clock/clock.py
@@ -9,6 +9,31 @@ except:
     import os  # для остальных систем
 
 
+def clock():
+    date_time = datetime.now().strftime('%d-%m-%Y %H:%M:%S/%p')
+    date, time1 = date_time.split()
+    time2, time3 = time1.split('/')
+    hour, minute, second = time2.split(':')
+    if 12 < int(hour) < 24:  # если 16:23:44, сделать
+        time = f'{str(int(hour) - 12)}:{minute}:{second} {time3}'
+        # 04:23:44 PM
+    else:
+        time = f'{time2} {time3}'  # 02:55:13 PM
+    time_label.config(text=time)
+    date_label.config(text=date)
+    time_label.after(1000, clock)
+
+
+def alarm():
+    main_time = datetime.now().strftime('%H:%M %p')
+    alarm_time = get_alarm_time_entry.get()
+    alarm_time1, alarm_time2 = alarm_time.split()
+    alarm_hour, alarm_minute = alarm_time1.split(':')
+    if 12 < int(alarm_hour) < 24:
+        main_hour = f'{str(int(alarm_hour) - 12)}'
+    else:
+        main_hour = alarm_hour
+
 root = Tk()
 root.title('Digital clock')  # название приложения (отображается в окошке наверху)
 root.geometry('500x250')  # размер окна
@@ -28,13 +53,25 @@ tabs_control.pack(expand=1, fill='both')
 
 # Делаем часы
 time_label = Label(clock_tab, font='calibri 40 bold',
-                   foreground='green', background='black')
+                   foreground='#54ff9f', background='black')
 # ^ привязываю лейбл к вкладке, настраиваю шрифт (40пт, жирный), указываю цвет текста, цвет фона
 time_label.pack(anchor='center')  # упаковать с привязкой к центру
 date_label = Label(clock_tab, font='calibri 40 bold',
-                   foreground='green', background='black')
+                   foreground='#54ff9f', background='black')
 date_label.pack(anchor='s')  # опускался вниз
 
 
+# делаем будильник
+get_alarm_time_entry = Entry(alarm_tab, font='calibri 15 bold')  # поле для ввода времени, когда вас нужно разбудить
+get_alarm_time_entry.pack(anchor='center')
+alarm_label = Label(alarm_tab, font='calibri 20 bold',
+                    text='Укажите время подъема в формате:\n07:30 AM\nЧасы: 07, Минуты: 30')
+alarm_label.pack(anchor='s')
+alarm_btn = Button(alarm_tab, text='Завести будильник!', command='')
+alarm_btn.pack(anchor='s')
+alarm_status = Label(alarm_tab, font='calibri 15 bold')
+alarm_status.pack(anchor='s')
+
 if __name__ == "__main__":
+    clock()
     root.mainloop()
